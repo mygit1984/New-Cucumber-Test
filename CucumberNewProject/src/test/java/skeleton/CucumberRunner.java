b657c495f5f1d88d03df68b8448bcd938bbe3408
kannan.ramalingam <kannan.ramalingam@BDC4-L-5LW6QQ2.dir.svc.accenture.com>
2019-09-17 14:43:30 +0530

    2nd commit

--- CucumberNewProject/src/test/java/skeleton/CucumberRunner.java
+++ CucumberNewProject/src/test/java/skeleton/CucumberRunner.java
@@ -1,11 +1,8 @@
 package skeleton;
 
 import org.junit.runner.RunWith;
-
-//import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin= {"pretty","json:src/cucumber-reports/report1.json"},monochrome=true,
